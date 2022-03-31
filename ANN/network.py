1ebb64b883d69dbb4d34d7cfc8df122c9a2f9a83
JochiSt <jochen.steinmann@web.de>
2022-03-31 08:27:45 +0200

    added function to save model

--- ANN/network.py
+++ ANN/network.py
@@ -10,7 +10,10 @@ from tensorflow import keras
 # number of samples for training, testing etc.
 SAMPLES = 100000
 
-def generate_network(name="network_v0.4"):
+def create_model(name="network_v0.4"):
+    """
+        
+    """
 
     inputs = keras.Input(shape=(128,), name="waveform_input")
 
@@ -73,4 +76,31 @@ def generate_network(name="network_v0.4"):
     # Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
     model_1.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
+    # save an image of the ANN
+    tf.keras.utils.plot_model(model_1, 
+            to_file='model_1.png',        # output file name
+            show_layer_activations=True,  # show activation functions
+            show_layer_names=True,        # show layer names
+            show_dtype=True,              # show datatype
+            show_shapes=True,             # show input / output shapes
+            rankdir='LR'                  # left to right image
+        )
+
     return  model_1
+
+
+def save_model(model, name=None):
+    """
+        Save as model.h5, model_weights.h5, and model.json
+
+        from https://jiafulow.github.io/blog/2021/02/17/simple-fully-connected-nn-firmware-using-hls4ml/
+    """
+
+    if name is None:
+        name = model.name
+
+    model.save(name + '.h5')
+    model.save_weights(name + '_weights.h5')
+    with open(name + '.json', 'w') as outfile:
+        outfile.write(model.to_json())
+    return
