a1daefeb4e11cbbf7bc66e5c389028385858a43a
JochiSt <jochen.steinmann@web.de>
2022-05-02 12:13:24 +0200

    added some plots and enhanced the analysis

--- SineTest/studies/impact_post_train_quant.py
+++ SineTest/studies/impact_post_train_quant.py
@@ -28,7 +28,10 @@ def impact_post_train_quant(modelfilename, NSAMPLES=1000):
         'ap_fixed<13,6>',
         'ap_fixed<12,6>',
         'ap_fixed<11,6>',
-        'ap_fixed<10,6>'
+        'ap_fixed<10,6>',
+        'ap_fixed<9,6>',
+        'ap_fixed<8,6>',
+        'ap_fixed<7,6>',
         ]
 
     # generate data for evaluation
@@ -37,6 +40,17 @@ def impact_post_train_quant(modelfilename, NSAMPLES=1000):
 
     distances = np.array([])
 
+    fig, ax1 = plt.subplots()
+    ax1.plot(x_test, y_truth, '.', label='Truth', markersize=5)
+
+    # load Keras Model
+    model = load_model(modelfilename)
+    y_keras = model.predict( x_test )
+    mad_keras = meanAbsDistance( y_truth, y_keras )
+
+    ax1.plot(x_test, y_keras, '.', label='Keras', markersize=5)
+
+    del model
 
     for quant in quantisations:
         # load Keras Model
@@ -54,11 +68,39 @@ def impact_post_train_quant(modelfilename, NSAMPLES=1000):
         for x in x_test:
             y_hls = np.append(y_hls, hls_model.predict( np.array(x) ))
 
+        ax1.plot( x_test, y_hls, '.', label=quant, markersize=5 )
+
         mad = meanAbsDistance( y_truth, y_hls )
 
         print(quant, mad)
 
         distances = np.append( distances, mad )
+
+    ax1.set_xlabel('x values')
+    ax1.set_ylabel('y values')
+
+    ax1.set_ylim([-1.5, 1.5])
+
+    plt.title('Comparison between Keras and HLS4ML')
+    fig.legend()
+    plt.savefig(model.name+"_PostQuantEffect_CMP.png")
+    plt.show()
+
+    fig, ax1 = plt.subplots()
+    x = np.arange(0, len(quantisations))
+    ax1.plot( distances, label='Mean Absolute Distance')
+    ax1.axhline(y=mad_keras, color='r', linestyle='-', label="Keras")
+
+    # Set number of ticks for x-axis
+    ax1.set_xticks(x)
+    # Set ticks labels for x-axis
+    ax1.set_xticklabels( quantisations, fontsize=11)
+    ax1.set_ylabel('Mean Absolute Difference')
+
+    plt.title('Impact of Post Training Quantisation HLS4ML')
+    fig.legend()
+    plt.savefig(model.name+"_PostQuantEffect.png")
+    plt.show()
 
 
 if __name__ == "__main__":
