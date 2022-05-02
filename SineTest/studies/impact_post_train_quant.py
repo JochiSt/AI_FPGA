24afc60f11954bbff5fa7448cec611dc52f423c1
JochiSt <jochen.steinmann@web.de>
2022-05-02 11:45:15 +0200

    added script to study the post quantisation impact

---
+++ SineTest/studies/impact_post_train_quant.py
@@ -0,0 +1,65 @@
+"""
+    evaluate the impact of post training quantisation
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from print_dict import print_dict
+
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import hls4ml
+
+import sys
+import sys
+sys.path.append('../')
+from network import generate_data, truth_function
+from evaluateHLSmodel import convert2HLS, createKerasConfig
+from compareModel import meanAbsDistance
+
+def impact_post_train_quant(modelfilename, NSAMPLES=1000):
+
+    quantisations = [
+        'ap_fixed<18,6>',
+        'ap_fixed<17,6>',
+        'ap_fixed<16,6>',
+        'ap_fixed<15,6>',
+        'ap_fixed<14,6>',
+        'ap_fixed<13,6>',
+        'ap_fixed<12,6>',
+        'ap_fixed<11,6>',
+        'ap_fixed<10,6>'
+        ]
+
+    # generate data for evaluation
+    x_test, y_test = generate_data(NSAMPLES)
+    y_truth = truth_function( x_test )
+
+    distances = np.array([])
+
+
+    for quant in quantisations:
+        # load Keras Model
+        model = load_model(modelfilename)
+
+        # create HLS4ML config
+        config = createKerasConfig(model)
+        config['Model']['Precision'] = quant
+
+        # convert model 2 HLS4ML
+        hls_model = convert2HLS(model, config)
+
+        # evaluate model with given config
+        y_hls = np.array([])
+        for x in x_test:
+            y_hls = np.append(y_hls, hls_model.predict( np.array(x) ))
+
+        mad = meanAbsDistance( y_truth, y_hls )
+
+        print(quant, mad)
+
+        distances = np.append( distances, mad )
+
+
+if __name__ == "__main__":
+    impact_post_train_quant('../storedANN/sine_v0.1.h5')
