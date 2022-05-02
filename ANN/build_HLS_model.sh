bb93dd38a5ec5cdcd9b7492a93a9b4511108fc19
JochiSt <jochen.steinmann@web.de>
2022-05-02 11:51:10 +0200

    also use faketime here

--- ANN/build_HLS_model.sh
+++ ANN/build_HLS_model.sh
@@ -1,2 +1,6 @@
 export PATH="$PATH:/opt/Xilinx/Vivado/2020.1/bin/"
-hls4ml build -p ann-hls-test -a
+faketime -f -1y hls4ml build -p ann-hls-test --simulation --synthesis --export
+#--vivado-synthesis
+
+# --co-simulation does not work
+# --validation needs co-sim
