63f486663987bffc8e1f569a9ff582a12606740f
JochiSt <jochen.steinmann@web.de>
2022-05-05 17:01:13 +0200

    updated test script, which should evaluate the ANN on the FPGA

--- SineTest/FPGA_evaluation/FPGA_evaluation.py
+++ SineTest/FPGA_evaluation/FPGA_evaluation.py
@@ -36,32 +36,76 @@ def FPGA_evaluation(tty="/dev/ttyUSB1"):
         serial_port.close()
         return
 
+    # instantiate float ap_fixed converter
+    converter = Converter()
+
     # some debug info
     print("connected to AI test")
 	
     # get ANN from reset
     serial_port.write(str.encode("r"))
 	
-    TEST_SIZE = 10
+    ANN_stimul = np.array([])
+    ANN_return = np.array([])
     
+    TEST_SIZE = 10
     for i in range(TEST_SIZE):
+        stim = np.random.rand() * 2*np.pi
+        ANN_stimul = np.append(ANN_stimul, stim)
+        
+        uint_stim = converter.forward_conversion(input_data=stim, 
+                                    signed=True, total_bits=16, fractional_bits=10)
+                                
+        
+        #uint_stim = 0x3132
+        int_stim = int(uint_stim)
+        print("Simulus %f %d %X"%(stim, uint_stim, int_stim))
+        """
         try:
-            serial_port.write(str.encode("1"))
+            byte_stim = uint_stim.to_bytes(2, 'big')
+            print("%x %x"%(byte_stim[0], byte_stim[1]))
+            
+            serial_port.write(byte_stim[0])
+            time.sleep(0.1)
             raw1 = serial_port.read( size=1 )
-
-            serial_port.write(str.encode("2"))
+            print(raw1)
+            
+            time.sleep(0.2)
+            
+            serial_port.write(byte_stim[1])
+            time.sleep(0.1)
             raw2 = serial_port.read( size=1 )
+            print(raw2)
             
-            if i > 1:
-                print("ANN", raw2, raw1)
+            if i > 0:
+                raw1 = int.from_bytes(raw1, "big")
+                raw2 = int.from_bytes(raw2, "big")
+                
+                raw = (raw2 << 8) + raw1
+
+                print("ANN %x %x = %X" %(raw1, raw2, raw) )
+                
+                result = converter.backward_conversion(input_data=raw, 
+                                    total_bits=16, fractional_bits=10)[0]
+                        
+                ANN_return = np.append(ANN_return, result)
             
+            time.sleep(0.5)
         except Exception as e:
             print(e)
             serial_port.close()
             break
-
+        """
     # put ANN into reset
     serial_port.write(str.encode("r"))
+    serial_port.close()
+    
+    plt.title("ANN output")
+    plt.ylabel("ANN output")
+    plt.xlabel("ANN stimulus")
+    plt.scatter( ANN_stimul[:-1], ANN_return )
+    plt.savefig("FPGA_evaluation.png")
+    plt.show()
 
 def testConverter():
     converter = Converter()
@@ -73,5 +117,5 @@ def testConverter():
     print('Backward converted input', fractional_result)
 
 if __name__ == "__main__":
-    #FPGA_evaluation()
-    testConverter()
+    FPGA_evaluation()
+    #testConverter()
