195ad1e72320c676c3b36752e33b996368cd5ac1
JochiSt <jochen.steinmann@web.de>
2022-05-13 17:24:35 +0200

    implemented functions to create a very basic layer and model config

--- SineTest/create_JSON_config.py
+++ SineTest/create_JSON_config.py
@@ -10,28 +10,19 @@ def createProjectConfig():
     cfg['OutputDir'] = 'sinetest'
     cfg['ProjectName'] = 'sinetest'
     cfg['ClockPeriod'] = 10                 # 10 ns => 100MHz
-    #cfg['ClockPeriod'] = clock_period
     
     with open('project_cfg.json','w') as outfile:
         json.dump(cfg, outfile, indent=4, sort_keys=True)  
     
-#TODO implement this function        
 def createLayerConfig(model):
     # create config by layer name
-    config = hls4ml.utils.config_from_keras_model(model, granularity='name')
-                                                    
+    layer_cfg = hls4ml.utils.config_from_keras_model(model, granularity='name')
+    with open(model.name+'_layer_cfg.json','w') as outfile:
+        json.dump(layer_cfg, outfile, indent=4, sort_keys=True)
      
-#TODO implement this function
 def createModelConfig(model):
     # create basic config
     model_cfg = hls4ml.utils.config_from_keras_model(model, granularity='model')
-    model_cfg['Model'] = {}
-    model_cfg['Model']['ReuseFactor'] = 1
-    #model_cfg['Model']['Strategy'] = 'Resource'
-    model_cfg['Model']['Strategy'] = 'Latency'
-    model_cfg['Model']['Precision'] = 'ap_fixed<16,6>'
-    model_cfg['Model']['Precision'] = 'ap_fixed<16,6>'
-    
     with open(model.name+'_model_cfg.json','w') as outfile:
         json.dump(model_cfg, outfile, indent=4, sort_keys=True)
     
