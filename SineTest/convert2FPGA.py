f356a5fbb2bf534d696d7037fc1037f29a3662d5
JochiSt <jochen.steinmann@web.de>
2022-05-13 14:25:31 +0200

    load the additional configurtion from an external JSON

--- SineTest/convert2FPGA.py
+++ SineTest/convert2FPGA.py
@@ -11,6 +11,8 @@ os.environ['PATH'] = '/opt/Xilinx/Vivado/2020.1/bin:' + os.environ['PATH']
 import hls4ml
 from print_dict import print_dict
 
+import json
+
 # import own helper functions
 import AI_TF_helpers as helpers
 
@@ -19,25 +21,30 @@ import AI_TF_helpers as helpers
 #hls4ml.model.optimizer.OutputRoundingSaturationMode.rounding_mode = 'AP_RND'
 #hls4ml.model.optimizer.OutputRoundingSaturationMode.saturation_mode = 'AP_SAT'
 
-def convert2FPGA(model, clock_period=4, build=True, profiling=False, additional_cfg={}):
+def convert2FPGA(model, clock_period=4, build=True, profiling=False, use_additional_cfg=True):
     # create basic config
     if not profiling:
         model_cfg = hls4ml.utils.config_from_keras_model(model, granularity='model')
     else:
         model_cfg = hls4ml.utils.config_from_keras_model(model, granularity='name')
 
-    # general config
     model_cfg['Model'] = {}
     model_cfg['Model']['ReuseFactor'] = 1
     #model_cfg['Model']['Strategy'] = 'Resource'
     model_cfg['Model']['Strategy'] = 'Latency'
     model_cfg['Model']['Precision'] = 'ap_fixed<16,6>'
     model_cfg['Model']['Precision'] = 'ap_fixed<16,6>'
-
-    # include the external configuration
-    model_cfg = helpers.merge_dicts(model_cfg, additional_cfg)
-
-    # for profiling, the trace mode must be enabled
+    
+    # include external configuration
+    if use_additional_cfg:
+        try:
+            with open(model.name+'_layer_cfg.json') as data_file:
+                additional_cfg = json.load(data_file)
+            model_cfg = helpers.merge_dicts(model_cfg, additional_cfg)
+        except Exception as e:
+            print(e)
+            pass
+    
     if profiling:
         for layer in model_cfg['LayerName'].keys():
             model_cfg['LayerName'][layer]['Trace'] = True
@@ -51,6 +58,7 @@ def convert2FPGA(model, clock_period=4, build=True, profiling=False, additional_
     cfg['KerasModel'] = model
     cfg['OutputDir'] = 'sinetest'
     cfg['ProjectName'] = 'sinetest'
+    #cfg['ClockPeriod'] = 10                 # 10 ns => 100MHz
     cfg['ClockPeriod'] = clock_period
 
     print("-----------------------------------")
@@ -86,9 +94,9 @@ if __name__ == "__main__":
     # use the already trained model
     from tensorflow.keras.models import load_model
     model = load_model('storedANN/sine_v0.2.h5')
-
+    
     convert2FPGA(model)
 
     # Check the reports
     hls4ml.report.read_vivado_report('sinetest')
-
+    
