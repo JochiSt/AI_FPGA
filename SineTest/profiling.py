9520fc1ca9a9203bbcff2e74ce22d811440f686f
JochiSt <jochen.steinmann@web.de>
2022-05-13 13:11:44 +0200

    played with the configuration for sine v0.2

--- SineTest/profiling.py
+++ SineTest/profiling.py
@@ -6,9 +6,45 @@ import hls4ml
 from convert2FPGA import convert2FPGA
 from network import generate_data
 
-def profiling_hlsmodel(model):
+from collections import defaultdict
+# from https://stackoverflow.com/questions/29348345/declaring-a-multi-dimensional-dictionary-in-python
+def nested_dict(n, type):
+    if n == 1:
+        return defaultdict(type)
+    else:
+        return defaultdict(lambda: nested_dict(n-1, type))
 
-    hls_model = convert2FPGA(model, clock_period=4, build=False, profiling=True)
+def profiling_hlsmodel(model):
+    
+    # create blank cfg
+    add_cfg = hls4ml.utils.config_from_keras_model(model, granularity='name')
+    
+    add_cfg['LayerName']['layer_0']['Precision']['weight'] = 'ap_fixed<6,1>'
+    add_cfg['LayerName']['layer_0']['Precision']['bias']   = 'ap_fixed<11,2>'
+    add_cfg['LayerName']['layer_0']['Precision']['result'] = 'ap_fixed<11,2>'
+    add_cfg['LayerName']['layer_0_linear']['Precision'] = add_cfg['LayerName']['layer_0']['Precision']['result']
+        
+    add_cfg['LayerName']['layer_1']['Precision']['weight'] = 'ap_fixed<11,2>'
+    add_cfg['LayerName']['layer_1']['Precision']['bias']   = 'ap_fixed<9,1>'
+    add_cfg['LayerName']['layer_1']['Precision']['result'] = 'ap_fixed<11,2>'
+    add_cfg['LayerName']['layer_1_linear']['Precision'] = add_cfg['LayerName']['layer_1']['Precision']['result']    
+    
+    add_cfg['LayerName']['output']['Precision']['weight']  = 'ap_fixed<7,2>'
+    add_cfg['LayerName']['output']['Precision']['bias']    = 'ap_fixed<4,1>'
+    add_cfg['LayerName']['output']['Precision']['result']  = 'ap_fixed<8,1>'
+    add_cfg['LayerName']['output_linear']['Precision'] = add_cfg['LayerName']['output']['Precision']['result']
+    
+    add_cfg['LayerName']['activation']  ['Precision'] = 'ap_fixed<11,2>'
+    add_cfg['LayerName']['activation_1']['Precision'] = 'ap_fixed<12,2>'
+
+    print(add_cfg)
+
+    hls_model = convert2FPGA(model, 
+                    clock_period=4, 
+                    build=False, 
+                    profiling=True, 
+                    additional_cfg = add_cfg
+                    )
     
     NSAMPLES = 1000
     # generate data for evaluation
