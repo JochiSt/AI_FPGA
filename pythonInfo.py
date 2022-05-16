69c720ac692c9b8d4c0e926b14a9fbfcb29bfe8b
JochiSt <jochen.steinmann@web.de>
2022-05-16 12:56:24 +0200

    added ignores to all the warnings
    this creates a nice printout for copy & paste

--- pythonInfo.py
+++ pythonInfo.py
@@ -7,11 +7,22 @@ def printVersions():
     from numpy import __version__
     print('Numpy ' + __version__)
 
+    # from https://stackoverflow.com/a/42121886
+    import os
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
     from tensorflow import __version__
     print('TensorFlow ' + __version__)
 
     from tensorflow.keras import __version__
     print('Keras ' + __version__)
+
+    from qkeras import __version__
+    print('QKeras' + __version__)
+
+    # from https://stackoverflow.com/a/53763710
+    import warnings
+    warnings.filterwarnings("ignore")
 
     from hls4ml import __version__
     print('HLS4ML ' + __version__)
